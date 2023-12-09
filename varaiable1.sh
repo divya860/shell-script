7d7c4bf85891faca4ba1fc8aa9a3d36c57b76f2e
disha <disha@123gmaile.com>
2023-12-09 23:56:08 +0530

    third

--- varaiable1.sh
+++ varaiable1.sh
@@ -2,7 +2,7 @@
 
 person1=ram
 person2=ragu
-echo "person1: hello person2"
-echo "person2: hello ,how are you"
-echo "person1: good and u"
-echo "person2: am also good"
+echo "$person1: hello $person2"
+echo "$person2: hello ,how are you"
+echo "$person1: good and u"
+echo "$person2: am also good"
