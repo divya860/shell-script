608f2266c5bb8aea4c6199a9c1e3c7eb0649b1d0
disha <disha@123gmaile.com>
2023-12-10 12:35:55 +0530

    file changes exiting file

--- variable2.sh
+++ variable2.sh
@@ -1,3 +1,8 @@
 #!bin/bash
 
-echo "Helloworld!"
+person1=ram
+person2=ragu
+echo "$person1: hello $person2"
+echo "$person2: hello ,how are you"
+echo "$person1: good and u"
+echo "$person2: am also good"
