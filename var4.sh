827116eccf0323fb7d496ad6e9f654bef6e30e3b
disha <disha@123gmaile.com>
2023-12-16 19:16:34 +0530

    file

---
+++ var4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "pls enter your username"
+
+read USERNAME 
+
+echo "username entered is: $USERNAME"
+
+echo "pls enter your password"
+
+read PASSWORD
+
+echo "passwoerd entered is" $PASSWORD"
