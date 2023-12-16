5c4909f2964db91e9e4bdc6c6bf733fcbf92e5ab
disha <disha@123gmaile.com>
2023-12-16 20:35:46 +0530

    file

--- specialvar.sh
+++ specialvar.sh
@@ -1,10 +1,19 @@
 #/bin/bash
 
-VAR1=$1
-VAR2=$2
+# VAR1=$1
+# VAR2=$2
 
-echo "scriptname : $0"
+# echo "scriptname : $0"
 
-echo "variable1 : $VAR1"
+# echo "variable1 : $VAR1"
 
-echo "all : $@"
+# echo "all : $@"
+
+
+echo 1
+
+echo 2
+
+echo 3
+
+echo 4
