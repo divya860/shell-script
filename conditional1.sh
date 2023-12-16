2030803ae4661be675c396b542d16492fbfc8417
disha <disha@123gmaile.com>
2023-12-16 19:49:49 +0530

    file

--- conditional1.sh
+++ conditional1.sh
@@ -4,9 +4,9 @@ NUMBER=$1
 
 if [ $NUMBER -gt 10 ]
 then
-    echo "$NUMBER is greaterthan 10"
+    echo "$NUMBER is greater than 10"
 
 else
-    echo "$NUMBER is not greaterthan 10"
+    echo "$NUMBER is not greater than 10"
 
 fi
