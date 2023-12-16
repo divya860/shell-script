a373d18ddf10e60e6f28dcc3ca69c2297e29c52f
disha <disha@123gmaile.com>
2023-12-16 20:01:06 +0530

    file

--- cond2.sh
+++ cond2.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-USERID = $(id -u)
+USERID=$(id -u)
 
 if [ $USERID -ne 0]
 
 then
-
-echo "pls run the script with root access"
+   echo "pls run the script with root access"
 
 fi
 
